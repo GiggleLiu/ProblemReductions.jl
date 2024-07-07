e62756d621030a566cf7b321f384c64631e4ab93
GiggleLiu <cacate0129@gmail.com>
2024-07-08 02:20:05 +0800

    circuit evaluation

--- test/circuit_expr.jl
+++ test/circuit_expr.jl
@@ -12,5 +12,11 @@ using ProblemReductions, Test
         d = x ∨ c
     end
     println(circuit)
-    @test ProblemReductions.evaluate(circuit, Dict(:x => true, :y => false, :c => false)) == Dict(:x => true, :y => false, :c => false, :d => true)
+    @test ProblemReductions.evaluate(circuit, Dict(:x => true, :y => false)) == Dict(:x => true, :y => false, :c => false, :d => true)
+    circuit = @circuit begin
+        c = x ∧ y
+        d = x ∨ (c ∧ ¬z)
+    end
+    println(circuit)
+    @test ProblemReductions.evaluate(circuit, Dict(:x => true, :y => false, :z => false)) == Dict(:x => true, :y => false, :z => false, :c => false, :d => true)
 end
