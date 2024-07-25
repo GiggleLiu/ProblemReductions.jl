8960106fe00cb51c3a9d1cca420c7a66b8d6b804
SciCodePhy <chenguang_guan@brown.edu>
2024-07-24 20:55:18 -0400

    Fix typo

--- src/models/SetCovering.jl
+++ src/models/SetCovering.jl
@@ -46,7 +46,7 @@ function set_covering_energy(sets::AbstractVector, weights::AbstractVector, conf
 end
 
 """
-    is_set_covering(sets::SetCovering, config)
+    is_set_covering(c::SetCovering, config)
 
 Return true if `config` (a vector of boolean numbers as the mask of sets) is a set covering of `sets`.
 """
