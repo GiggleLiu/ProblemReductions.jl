8dec5d5286837c8ed29eff9fcad65f1040956ee1
c-allergic <xli445614@gmail.com>
2024-07-25 15:19:52 +0800

    Save

--- examples/dining.jl
+++ examples/dining.jl
@@ -19,4 +19,17 @@ maxcut = MaxCut(graph)
 
 # ## From circuit satisfiability to cryptographic problems
 
-# ## Conclusion: Inviting friends is as hard as solving cryptographic problems
+# ## Inviting friends to a dinner is harder than cracking the encryption system
+# Intro to RSA encryption system
+# Intro to factoring problem
+# Example: how to solve $x \times y = 6$, by reducing to spin-glass
+arr = ProblemReductions.compose_multiplier(2, 2)
+ProblemReductions.set_output!(arr, [0, 1, 1, 0])  # ? x ? == 6
+
+# Spin-Glass to MaxCut (inviting friends to a dinner)
+
+# NOTE: the first/second argument is the bit-width of the first/second input.
+# TODO: visualize the spin-galss
+# https://queracomputing.github.io/UnitDiskMapping.jl/notebooks/tutorial.html
+# https://github.com/GiggleLiu/LuxorGraphPlot.jl
+# https://arxiv.org/abs/2209.03965
