c3181c02de932d4992919864a533ca4fd5a5e79a
c-allergic <xli445614@gmail.com>
2024-08-02 15:09:21 +0800

    save

--- examples/dining.jl
+++ examples/dining.jl
@@ -1,25 +1,32 @@
 # # Dining with Friends(developing yet)
 # ---
 # ## Inviting friends to dinner > cracking a bank encryption system
-# Using this package, we could showcase the problem how inviting friends to a dinner party is harder than cracking a bank encryption system.Let's introduce some background knowledge
-# ### Intor to RSA 
-# RSA is a public-key cryptosystem. It's widely used in 
+# Using this package, we could showcase the problem how inviting friends to a dinner party is harder than cracking a bank encryption system.Let's introduce some background knowledge.
 
-# ### Intro to factoring problem
-# The factoring problem is to find the prime factors of a composite number. It's a pratically hard problem. 
-# ### Reduction path from _Factoring_ to _MaxCut_
-# explain how I could use the ProblemReductions.jl to reduce the factoring problem to the MaxCut problem
+# ### Intor to RSA 
+# RSA is a public-key cryptosystem. It's widely used in encryption algorithm that helps secure bank transactions and communications. 
+# Here's the Introduction->[RSA encryption](https://en.wikipedia.org/wiki/RSA_(cryptosystem)). Easily explained, there are public key $(n,e)$ and private key$(n,d)$. 
+# The attacker needs to factorize the $n$, a product of two large prime numbers. So the security of RSA comes from factoring problem.
+# ### Factoring problem
+# The factoring problem is to find the prime factors of a composite number. It's a pratically hard problem. Generally, the input size in RSA is 2048 bits.
+# Consider two algorithms to solve it: General number field sieve(GNFS, a good algorithm in factoring) and  Brute force. 
+
+# | Algorithm | Time complexity | Operations | Time ($10^{12}$ ops/s)  |
+# | :--- | :--- | :--- | :--- | 
+# | GNFS | $O(e^{(\log n)^{1/3}(\log \log n)^{2/3}})$ | $2^{112}$ | $≈ 7 \times 10^{20}$ years |
+# | Brute force | $O(\sqrt{n})$ | $2^{1024}$|$≈5.7 \times 10^{228}$ years |
+# Both are way longer than the age of our universe.
+
+# So basically, RSA relies on factoring and if we could reduce factoring to maxcut problem, we could show that inviting friends to a dinner party is harder than cracking a bank encryption system.  
+# Next part, I'll reduce factoring to the maxcut by: Factoring -> Circuit Sat -> SpinGlass -> MaxCut.
 
 # ---
 # ## Factoring -> MaxCut
 using ProblemReductions, Graphs, LuxorGraphPlot
-
+# For a given number $n$, we could create a factoring problem.
 # ### Create a factoring problem
-# Example: how to solve $x \times y = 6$, by reducing to spin-glass
-# arr = ProblemReductions.compose_multiplier(2, 2)
-# ProblemReductions.set_output!(arr, [0, 1, 1, 0]) ->? x ? == 6
-
 # ### reduce the factoring to the circuit Sat problem
+
 # could I visualize this circuit Sat?
 # ### reduce the circuit Sat problem to the SpinGlass problem
 # Visualize this SpinGlass problem
