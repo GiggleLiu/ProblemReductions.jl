d85227b92daf2ce3f32c1189ac2ed0f50d662a8c
GiggleLiu <cacate0129@gmail.com>
2024-08-06 16:20:32 +0800

    update

--- src/rules/factoring_sat.jl
+++ src/rules/factoring_sat.jl
@@ -3,28 +3,28 @@ function reduceto(::Type{<:CircuitSAT}, f::Factoring)
     n1, n2, z = f.m, f.n, f.z
     p = [BooleanExpr(Symbol("p$i")) for i in 1:n1]
     q = [BooleanExpr(Symbol("q$i")) for i in 1:n2]
-    m = [BooleanExpr(Symbol("z$i")) for i in 1:n1+n2]
+    m = [BooleanExpr(Symbol("m$i")) for i in 1:n1+n2]
     others = BooleanExpr[]
     exprs = Assignment[]
-    s = [BooleanExpr(Symbol("s$i$j")) for i in 1:n1+1, j in 0:n2]
-    c = [BooleanExpr(Symbol("c$i$j")) for i in 0:n1, j in 1:n2]
-    # initialize s
-    for k = 1:size(s, 1)
-        push!(exprs, Assignment([s[k, 1]], BooleanExpr(:false)))
-    end
-    # initialize c
-    for k = 1:size(c, 2)
-        push!(exprs, Assignment([c[1, k]], BooleanExpr(:false)))
-    end
     for i in 1:n1
+        spres = BooleanExpr[]
+        cpre = BooleanExpr(:false)
         for j in 1:n2
+            # spre: the signal from the previous cycle
+            # cpre: the signal from the previous computational step
             # s + 2c = p*q + s_pre + c_pre
-            mul_exprs, ancillas = multiplier(s[i, j+1], c[i+1, j], p[i], q[j], s[i+1, j], c[i, j])
+            c = BooleanExpr(Symbol("c$i$j"))
+            s = BooleanExpr(Symbol("s$i$j"))
+            mul_exprs, ancillas = multiplier(s, c, p[i], q[j], j == n2 ? BooleanExpr(:false) : spres[j+1], cpre)
             append!(exprs, mul_exprs)
             append!(others, ancillas)
+            cpre = c
+            push!(spres, s)
+            push!(others, c)
+            push!(others, s)
         end
     end
-    return CircuitSAT(Circuit(exprs), vcat(pvars, qvars, outputs, vec(s), vec(c), others))
+    return CircuitSAT(Circuit(exprs), vcat(pvars, qvars, outputs, others))
 end
 
 function multiplier(s::BooleanExpr, c::BooleanExpr, p::BooleanExpr, q::BooleanExpr, spre::BooleanExpr, cpre::BooleanExpr)
