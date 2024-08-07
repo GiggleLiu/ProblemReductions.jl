ab82b349cbe0cf3366b8db9d4eb793909ac6bc57
c-allergic <xli445614@gmail.com>
2024-08-07 17:32:10 +0800

    new framework

--- examples/dining.jl
+++ examples/dining.jl
@@ -3,7 +3,7 @@
 # ## Inviting friends to dinner > cracking a bank encryption system
 # Using this package, we could showcase the problem how inviting friends to a dinner party is harder than cracking a bank encryption system.Let's introduce some background knowledge.
 
-# ### Intor to RSA 
+# ### Intro to RSA 
 # RSA is a public-key cryptosystem. It's widely used in encryption algorithm that helps secure bank transactions and communications. 
 # Here's the Introduction->[RSA encryption](https://en.wikipedia.org/wiki/RSA_(cryptosystem)). Easily explained, there are public key $(n,e)$ and private key$(n,d)$. 
 # The attacker needs to factorize the $n$, a product of two large prime numbers. So the security of RSA comes from factoring problem.
@@ -22,13 +22,20 @@
 
 # ---
 # ## Factoring -> MaxCut
+# To start with, we import the necessary packages.
 using ProblemReductions, Graphs, LuxorGraphPlot
-# For a given number $n$, we could create a factoring problem.
 # ### Create a factoring problem
+n = 15
+mul = compose_multiplier(4,4)
 # ### reduce the factoring to the circuit Sat problem
-
-# could I visualize this circuit Sat?
+# Circuit Sat, easily explained, is a circuit with some inputs and outputs and the circuit contains some logical constraints like $\land$ and $\lor$. The goal is to find the inputs that make the output true.
+# So here we wants to verify that the product of 2 prime numbers is 15. Then we could set the constraints to the outcome to ensure it's 15.
+# 这里用乘法器的话直接就有一个spinglass了，不用再转化了。
 # ### reduce the circuit Sat problem to the SpinGlass problem
+sg = mul.problem
 # Visualize this SpinGlass problem
 # ### reduce the SpinGlass problem to the MaxCut problem
+mc = reduceto(MaxCut, sg)
+MC = mc.maxcut
+LuxorGraphPlot.show_graph(MC.graph, edge_labels=MC.weights)
 # Visualize this MaxCut problem and explain how the maxcut problem represents inviting friends to a dinner party
