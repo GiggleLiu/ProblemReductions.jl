62430c466d5e9760cf04e06a69fe7286a78bf7dc
GiggleLiu <cacate0129@gmail.com>
2024-08-12 14:14:26 +0800

    add test for reduction path

--- test/reduction_path.jl
+++ test/reduction_path.jl
@@ -11,7 +11,12 @@ using ProblemReductions, Test, Graphs
 
     paths = reduction_paths(MaxCut, QUBO)
     @test length(paths) >= 1
-    res = implement_reduction_path(g, paths[1], MaxCut(smallgraph(:petersen)))
+    source = MaxCut(smallgraph(:petersen))
+    res = implement_reduction_path(g, paths[1], source)
     @test target_problem(res) isa QUBO
     @test reduction_complexity(res) == 1
+
+    best1 = findbest(source, BruteForce())
+    best2 = findbest(target_problem(res), BruteForce())
+    @test sort(extract_solution.(Ref(res), best2)) == sort(best1)
 end
