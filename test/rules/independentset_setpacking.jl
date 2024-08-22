bbfde779234a197b8e060c639bea05afffaf46c7
SciCodePhy <chenguang_guan@brown.edu>
2024-08-22 17:16:51 -0400

    Add more reduction tests for AbstractGraph

--- test/rules/independentset_setpacking.jl
+++ test/rules/independentset_setpacking.jl
@@ -22,10 +22,20 @@ using Test, ProblemReductions, Graphs
     add_edge!(g02, 2, 3)
     add_edge!(g02, 3, 4)
 
+    hg = HyperGraph(5, [[1, 2], [2, 3, 4]])
+    udg = UnitDiskGraph([(0.0, 0.0), (1.0, 0.0), (0.0, 1.0), (1.0, 1.0)], 1.2)
+    gg = GridGraph(Bool[1 0; 1 1], 1.2)
+
     IS_01 = IndependentSet(g01)
     IS_02 = IndependentSet(g02)
+    IS_hg = IndependentSet(hg)
+    IS_udg = IndependentSet(udg)
+    IS_gg = IndependentSet(gg)
 
     @test reduction_complexity(SetPacking, IS_01) == 1
     @test verify(IS_01)
     @test verify(IS_02)
+    @test verify(IS_hg)
+    @test verify(IS_udg)
+    @test verify(IS_gg)
 end
