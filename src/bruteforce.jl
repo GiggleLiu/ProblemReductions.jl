0fdea7d04169da408a04ee97d990d512e3004262
GiggleLiu <cacate0129@gmail.com>
2024-12-14 01:07:18 +0800

    minor fix

--- src/bruteforce.jl
+++ src/bruteforce.jl
@@ -3,23 +3,26 @@ $TYPEDEF
 
 A brute force method to find the best configuration of a problem.
 """
-struct BruteForce end
+Base.@kwdef struct BruteForce{T}
+    atol::T=eps(Float64)
+    rtol::T=eps(Float64)
+end
 
-function Base.findmin(problem::AbstractProblem, method; atol=eps(Float64), rtol=eps(Float64))
-    findall(problem, method; atol, rtol, initial=SolutionSize(Inf, false), by=(x, y) -> x.is_valid && (!y.is_valid || isless(x.size, y.size)))
+function Base.findmin(problem::AbstractProblem, method)
+    findall(problem, method; initial=SolutionSize(Inf, false), by=(x, y) -> x.is_valid && (!y.is_valid || isless(x.size, y.size)))
 end
-function Base.findmax(problem::AbstractProblem, method; atol=eps(Float64), rtol=eps(Float64))
-    findall(problem, method; atol, rtol, initial=SolutionSize(-Inf, false), by=(x, y) -> x.is_valid && (!y.is_valid || isless(y.size, x.size)))
+function Base.findmax(problem::AbstractProblem, method)
+    findall(problem, method; initial=SolutionSize(-Inf, false), by=(x, y) -> x.is_valid && (!y.is_valid || isless(y.size, x.size)))
 end
-function findbest(problem::AbstractProblem, method; atol=eps(Float64), rtol=eps(Float64))
-    energy_mode(problem) == LargerSizeIsBetter() ? findmax(problem, method; atol, rtol) : findmin(problem, method; atol, rtol)
+function findbest(problem::AbstractProblem, method)
+    energy_mode(problem) == LargerSizeIsBetter() ? findmax(problem, method) : findmin(problem, method)
 end
-function Base.findall(problem::AbstractProblem, ::BruteForce; atol=eps(Float64), rtol=eps(Float64), initial=SolutionSize(0.0, false), by=isless)
+function Base.findall(problem::AbstractProblem, bf::BruteForce; initial=SolutionSize(0.0, false), by=isless)
     flvs = flavors(problem)
     best_ids = NTuple{num_variables(problem), Int}[]
     configs = Iterators.product([1:length(flvs) for i in 1:num_variables(problem)]...)
     sizes = solution_size_byid(problem, configs)
-    _find!(by, best_ids, configs, sizes, initial, atol, rtol)
+    _find!(by, best_ids, configs, sizes, initial, bf.atol, bf.rtol)
     return [collect(id_to_config(problem, id)) for id in best_ids]
 end
 
