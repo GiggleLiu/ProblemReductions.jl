618343f3a1f7fb8c2930661ef6cdfd15af655b25
Zhongyi <zni573@connect.hkust-gz.edu.cn>
2025-03-17 18:30:10 +0800

    hypercube

--- ext/IPSolverExt.jl
+++ ext/IPSolverExt.jl
@@ -1,23 +1,58 @@
 module IPSolverExt
 
-using JuMP, ProblemReductions
+import JuMP
+using ProblemReductions
+using LinearAlgebra
+using Combinatorics
 
+include("combinations.jl")
+include("hypercube.jl")
 function Base.findmin(problem::AbstractProblem, solver::IPSolver)
+    @assert num_flavors(problem) == 2 "findmin only supports boolean variables"
     cons = constraints(problem)
+    nsc = ProblemReductions.num_variables(problem)
+    maxN = maximum([length(c.variables) for c in cons])
+    @assert maxN <= 5 "findmin only supports constraints with at most 5 variables"
+    cuts = [all_sets_cuts(i) for i in 2:maxN]
+
     objs = objectives(problem)
+    @assert all(length(obj.variables) <= 1 for obj in objs) "findmin only supports objectives with at most 1 variables"
+
     # IP by JuMP
-    model = Model(solver.optimizer)
-    !solver.verbose && set_silent(model)
+    model = JuMP.Model(solver.optimizer)
+    !solver.verbose && JuMP.set_silent(model)
 
-    @variable(model, 0 <= x[i = 1:nsc] <= 1, Int)
-    @objective(model, Min, sum(x[i] * weights[i] for i in 1:nsc))
-    for i in 1:num_items
-        @constraint(model, sum(x[j] for j in sets_id[i]) >= 1)
+    JuMP.@variable(model, 0 <= x[i = 1:nsc] <= 1, Int)
+    # @objective(model, Max, sum(x[i] for i in 1:nsc))
+    for con in cons
+        subsets,ies = cuts[length(con.variables)-1]
+        coverset = findall(!,con.specification)
+        subset_pos = findall(set -> issubset(set, coverset),subsets)
+        result = minimal_set_cover(coverset, subsets[subset_pos], solver.optimizer)
+        for i in result
+            ie = ies[subset_pos[i]]
+            # 1: <=, 2: >=, 3: < , 4: >
+            if ie.symb == 1
+                JuMP.@constraint(model, sum(j-> x[con.variables[j]]*ie.hp.coefficients[j] , 1:length(con.variables)) <= ie.hp.offset)
+            elseif ie.symb == 2
+                JuMP.@constraint(model, sum(j-> x[con.variables[j]]*ie.hp.coefficients[j] , 1:length(con.variables)) >= ie.hp.offset)
+            elseif ie.symb == 3
+                JuMP.@constraint(model, sum(j-> x[con.variables[j]]*ie.hp.coefficients[j] , 1:length(con.variables)) <= ie.hp.offset-1e-2)
+            elseif ie.symb == 4
+                JuMP.@constraint(model, sum(j-> x[con.variables[j]]*ie.hp.coefficients[j] , 1:length(con.variables)) >= ie.hp.offset+1e-2)
+            end
+        end
     end
+    
+    obj_sum = 0
+    for obj in objs
+        obj_sum += x[obj.variables[1]]*obj.specification[1] + (1-x[obj.variables[1]])*obj.specification[2]
+    end
+    JuMP.@objective(model,Min, obj_sum)
 
-    optimize!(model)
-    @assert is_solved_and_feasible(model)
-    return round.(Int, value.(x))
+    JuMP.optimize!(model)
+    @assert JuMP.is_solved_and_feasible(model)
+    return round.(Int, JuMP.value.(x))
 end
 
 function minimal_constraints(nflavor::Int, set::Vector)
@@ -44,12 +79,12 @@ function minimal_set_cover(coverset::Vector{Int}, subsets::Vector{Vector{Int}}, 
     @assert all(set -> issubset(set, coverset), subsets) "subsets ($subsets) must not cover any elements absent in the coverset ($coverset)"
 
     # Create a JuMP model for exact set cover
-    model = Model(optimizer)
-    !verbose && set_silent(model)
+    model = JuMP.Model(optimizer)
+    !verbose && JuMP.set_silent(model)
 
     # Define binary variables for each subset (1 if selected, 0 if not)
     n = length(subsets)
-    @variable(model, x[1:n], Bin)
+    JuMP.@variable(model, x[1:n], Bin)
     
     # Each element in the coverset must be covered exactly once
     for element in coverset
@@ -57,17 +92,17 @@ function minimal_set_cover(coverset::Vector{Int}, subsets::Vector{Vector{Int}}, 
         covering_subsets = [i for i in 1:n if element in subsets[i]]
         
         # Each element must be covered exactly once
-        @constraint(model, sum(x[i] for i in covering_subsets) >= 1)
+        JuMP.@constraint(model, sum(x[i] for i in covering_subsets) >= 1)
     end
     
     # Minimize the number of subsets used (optional objective)
-    @objective(model, Min, sum(x))
+    JuMP.@objective(model, Min, sum(x))
     # Solve the model
-    optimize!(model)
+    JuMP.optimize!(model)
     
     # Return the solution if feasible
-    if termination_status(model) == MOI.OPTIMAL
-        return [i for i in 1:n if value(x[i]) > 0.5]
+    if  JuMP.termination_status(model) ==  JuMP.MOI.OPTIMAL
+        return [i for i in 1:n if  JuMP.value(x[i]) > 0.5]
     else
         error("No solution found")
     end
--- test/IPSolverExt.jl
+++ test/IPSolverExt.jl
@@ -16,3 +16,80 @@ using SCIP
     @test result == [1, 2, 4] || result == [1, 3, 4]
 end
 
+using Graphs
+graph = smallgraph(:petersen)
+problem = MaximalIS(graph)
+findmin(problem, IPSolver(SCIP.Optimizer,20,false))
+
+@testset "HyperPoint" begin
+    Ext = Base.get_extension(ProblemReductions, :IPSolverExt)
+    point1 = Ext.HyperCubePoint((true,false,true))
+    point2 = Ext.HyperCubePoint([true, false, true])
+    @test point1 == point2
+
+    @test length(Ext.all_points(3)) == 8
+end
+
+@testset "point_on_plane" begin
+    Ext = Base.get_extension(ProblemReductions, :IPSolverExt)
+    plane = Ext.HyperPlane((1.0, 2.0, -3.0), 1.0)
+    @test Ext.point_on_plane(Ext.HyperCubePoint((true,false,false)), plane) == 0
+    @test Ext.point_on_plane(Ext.HyperCubePoint((false,false,true)), plane) == -4
+    @test Ext.point_on_plane(Ext.HyperCubePoint((false,true,false)), plane) == 1
+end
+
+@testset "HyperCubePlane" begin
+    Ext = Base.get_extension(ProblemReductions, :IPSolverExt)
+    p1 = Ext.HyperCubePoint((true,false,false))
+    p2 = Ext.HyperCubePoint((false,false,true))
+    p3 = Ext.HyperCubePoint((false,true,false))
+    plane = Ext.HyperPlane([p1, p2, p3])
+    @test Ext.point_on_plane(p1, plane) == 0
+    @test Ext.point_on_plane(p2, plane) == 0
+    @test Ext.point_on_plane(p3, plane) == 0
+
+    p4 = Ext.HyperCubePoint((true,true,true))
+    p5 = Ext.HyperCubePoint((false,false,false))
+    p6 = Ext.HyperCubePoint((true,false,false))
+    plane = Ext.HyperPlane([p4, p5, p6])
+    @test Ext.point_on_plane(p4, plane) == 0
+    @test Ext.point_on_plane(p5, plane) == 0
+    @test Ext.point_on_plane(p6, plane) == 0
+
+    p7 = Ext.HyperCubePoint((false,false,false))
+    p8 = Ext.HyperCubePoint((false,true,false))
+    p9 = Ext.HyperCubePoint((false,false,true))
+    plane = Ext.HyperPlane([p7, p8, p9])
+    @test Ext.point_on_plane(p7, plane) == 0
+    @test Ext.point_on_plane(p8, plane) == 0
+    @test Ext.point_on_plane(p9, plane) == 0
+
+    p7 = Ext.HyperCubePoint((false,false,false))
+    p8 = Ext.HyperCubePoint((true,true,false))
+    p9 = Ext.HyperCubePoint((true,true,true))
+    plane = Ext.HyperPlane([p7, p8, p9])
+    @test Ext.point_on_plane(p7, plane) ≈ 0
+    @test Ext.point_on_plane(p8, plane) ≈ 0 atol = 1e-8
+    @test Ext.point_on_plane(p9, plane) ≈ 0 atol = 1e-8
+
+    # HyperCubePoint{4}[(0000), (1000), (0100), (1100)]
+    p1 = Ext.HyperCubePoint((false,false,false,false))
+    p2 = Ext.HyperCubePoint((true,false,false,false))
+    p3 = Ext.HyperCubePoint((false,true,false,false))
+    p4 = Ext.HyperCubePoint((true,true,false,false))
+    plane = Ext.HyperPlane([p1, p2, p3, p4])
+    @test isnothing(plane)
+end
+
+@testset "HyperCubePlaneCut" begin
+    Ext = Base.get_extension(ProblemReductions, :IPSolverExt)
+    p4 = Ext.HyperCubePoint((true,true,true))
+    p5 = Ext.HyperCubePoint((false,false,false))
+    p6 = Ext.HyperCubePoint((true,false,false))
+    plane = Ext.HyperPlane([p4, p5, p6])
+    pl = Ext.all_points(3)
+    hc = Ext.HyperCubePlaneCut(plane,pl)
+    @test hc.on == [1,2,7,8]
+    @test hc.above == [5,6]
+    @test hc.below == [3,4]
+end
